f7a8a293b1d5c2ccc676374147ed2025e58fa8be
g10-vr <vishal.11.r@gmail.com>
2014-03-30 22:31:41 -0700

    Deleted ~php and extra librarian files

--- Librarian/additemtobranch.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$con=mysqli_connect("localhost","root","");
-	// Check connection
-	if (mysqli_connect_errno()){
-	  echo "<br />Failed to connect to MySQL: " . mysqli_connect_error();
-	}
-
-	mysqli_query($con,"INSERT INTO Items
-?>
