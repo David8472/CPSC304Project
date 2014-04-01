d8302df2de0607b50b242ec756675ff43d8bffe9
Eric <ericwong3407@hotmail.com>
2014-03-31 22:59:33 -0700

    Fixed clerkOverdue comparison error

--- Clerk/ClerkReturn.php
+++ Clerk/ClerkReturn.php
@@ -5,10 +5,10 @@ $con=mysqli_connect("localhost","root","", "DBCPSC304");
 if (mysqli_connect_errno()){
   echo "<br />Failed to connect to MySQL: " . mysqli_connect_error();
 }
-//$callNumber = '$_POST[callNumber]';
-//$copyNumber = '$_POST[copyNo]';
-$callNumber = '1111';
-$copyNo = '1';
+$callNumber = '$_POST[callNumber]';
+$copyNumber = '$_POST[copyNo]';
+//$callNumber = '1111';
+//$copyNo = '1';
 $today = date('Y-m-d');
 
 // Check to see if book has been taken out
